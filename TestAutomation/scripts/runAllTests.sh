a6932dbd853ae0d98f80a1d80eeebce787009a4b
Dylan <mansourd@g.cofc.edu>
2016-10-23 18:33:38 -0400

    Update runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -6,3 +6,21 @@ do
         $SCRIPT
    fi
 done
+
+
+#!/bin/bash
+clear
+echo "---------Starting Script---------"
+echo
+echo "---------Generating Vars---------"
+OUTPUT="OutputFile/"
+FILE="examplePDF.pdf"
+echo "---------Making Directories---------"
+mkdir -p OutputFile/
+echo "---------Converting---------"
+java -Xmx512M&nbsp; -jar jpdf2html.jar $FILE $OUTPUT
+echo "-----Starting PNGQuant Script-----"
+find . -name '*.png' -exec pngquant --ext .png --force 256 {} \;
+echo "-----Finished PNGQuant Script-----"
+echo
+echo "---------Finished Script---------"
