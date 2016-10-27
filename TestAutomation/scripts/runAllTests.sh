d5d4b3aa25f796580404e7c4b8bca8c98dbdff9a
joetschell <joetschell@hotmail.com>
2016-10-27 09:37:20 -0400

    new script & updated testCases

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,26 +1,43 @@
+#!/bin/bash
 
-for SCRIPT in TestAutomation/scripts/*
+echo "changing directory to testCases..."
+cd testCases
+echo "reading test cases..."
+
+rm -r ../reports/*	#clear the reports directory
+echo "<!DOCTYPE html>" >>../reports/htmlTemplate.html
+echo  "<html>" >> ../reports/htmlTemplate.html
+echo "<head>" >> ../reports/htmlTemplate.html
+echo  "<title>Test Report</title>" >> ../reports/htmlTemplate.html
+echo  "</head>" >> ../reports/htmlTemplate.html
+echo  "<body>" >> ../reports/htmlTemplate.html
+echo  "<table border=1 style=width:75%>" >> ../reports/htmlTemplate.html
+echo  "<tr>" >> ../reports/htmlTemplate.html
+echo  "<th> Test # </th>">> ../reports/htmlTemplate.html
+echo  "<th> class name </th>">> ../reports/htmlTemplate.html
+echo  "<th> method name </th>">> ../reports/htmlTemplate.html
+echo  "<th> input </th>">> ../reports/htmlTemplate.html
+echo  "<th> expected output </th></tr>">> ../reports/htmlTemplate.html
+
+lineStorage=( )
+for filename in *
 do
-   if [ -f $SCRIPT -a -x $SCRIPT ]
-   then
-        $SCRIPT
-   fi
+echo "<tr>" >> ../reports/htmlTemplate.html
+   counter=0
+	while IFS='' read -r line;do
+	   lineStorage[$counter]="$line"
+	   if [ "$counter" -eq 0 ] || [ "$counter" -eq 3 ] || [ "$counter" -eq 5 ]
+	   then
+		echo "<td>" >> ../reports/htmlTemplate.html
+		echo ${lineStorage[$counter]} >> ../reports/htmlTemplate.html
+	   fi
+	((counter++))
+	        echo "</td>" >> ../reports/htmlTemplate.html
+	done < "$filename"
+
+echo "</tr>" >> ../reports/htmlTemplate.html
+echo ""
 done
 
-
-#!/bin/bash
-clear
-echo "---------Starting Script---------"
-echo
-echo "---------Generating Vars---------"
-OUTPUT="OutputFile/"
-FILE="examplePDF.pdf"
-echo "---------Making Directories---------"
-mkdir -p OutputFile/
-echo "---------Converting---------"
-java -Xmx512M&nbsp; -jar jpdf2html.jar $FILE $OUTPUT
-echo "-----Starting PNGQuant Script-----"
-find . -name '*.png' -exec pngquant --ext .png --force 256 {} \;
-echo "-----Finished PNGQuant Script-----"
-echo
-echo "---------Finished Script---------"
+cd ../reports
+xdg-open htmlTemplate.html
