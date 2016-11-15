9d895d97d1eae2c68cc0fa3cd5f258b96ecb343d
joetschell <joetschell@hotmail.com>
2016-11-15 01:06:47 -0500

    new script update

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -5,6 +5,7 @@ cd testCases
 echo "reading test cases..."
 
 rm -r ../reports/*	#clear the reports directory
+#sets up initial html Doc---------------------------------------------------
 echo "<!DOCTYPE html>" >>../reports/htmlTemplate.html
 echo  "<html>" >> ../reports/htmlTemplate.html
 echo "<head>" >> ../reports/htmlTemplate.html
@@ -20,14 +21,21 @@ echo  "<th> input </th>">> ../reports/htmlTemplate.html
 echo  "<th> output </th>">> ../reports/htmlTemplate.html
 echo  "<th> expected output </th>" >> ../reports/htmlTemplate.html
 echo  "<th> pass/fail </th></tr>">> ../reports/htmlTemplate.html
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
+
+
+testCaseTracker=1  #tracks what test case is being worked on
 lineStorage=( ) #array for storing lines from test case files
 for filename in *    #goes through all the files in testCase dir
 do
 echo "<tr>" >> ../reports/htmlTemplate.html
+   
    counter=0
 	while IFS='' read -r line;do #reads lines from a test Case file
 	   lineStorage[$counter]="$line"
+#0=TestCaseNumber, 2=ClassName, 3=MethodName, 4=input, 5=expectedOutput, 6=classpath, 7=DriverName
 	  if [ "$counter" -eq 0 ] || [ "$counter" -eq 2 ] || [ "$counter" -eq 3 ] || [ "$counter" -eq 4 ] 
 	   then
 		echo "<td>" >> ../reports/htmlTemplate.html
@@ -41,14 +49,32 @@ echo "<tr>" >> ../reports/htmlTemplate.html
 #cd ../project/src/api/target/classes
 cd ..
 
-output=$(echo ${lingStorage[$counter]} | java -classpath ${lineStorage[6]} org.openmrs.PersonNameDriverTest Nick)
-pwd
+#pipes input into java driver classes and puts them in html table
+output=$(echo ${lineStorage[4]} | java -classpath ${lineStorage[6]} org.openmrs.${lineStorage[7]})
 echo "<td>" >> reports/htmlTemplate.html
 echo $output >> reports/htmlTemplate.html
 echo "</td>" >> reports/htmlTemplate.html
 
+#prints out expected output from test case
+echo "<td>" >> reports/htmlTemplate.html
+echo ${lineStorage[5]}>> reports/htmlTemplate.html
+echo "</td>" >> reports/htmlTemplate.html
+
+#compares expected output to actual output and decides if test passed or failed
+echo "<td" >> reports/htmlTemplate.html
+if [ $output = ${lineStorage[5]} ];then
+   echo "style=\"color:#0D7010;\">Passed" >> reports/htmlTemplate.html
+else
+   echo "style=\"color:#FF0000;\">Failed" >> reports/htmlTemplate.html
+fi
+echo "</td>" >> reports/htmlTemplate.html
+
+
+
 
 echo "</tr>" >> reports/htmlTemplate.html #ends a row in the table
+echo "test case $testCaseTracker complete"
+((testCaseTracker++))
 cd testCases
 done
 
